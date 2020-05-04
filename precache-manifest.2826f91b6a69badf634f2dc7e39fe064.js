b198d14c7044a964f862016227bca47ffcdfa6c1
Pratik Darak <darak.pratik@gmail.com>
2020-05-04 19:31:03 +0530

    Updates

---
+++ precache-manifest.2826f91b6a69badf634f2dc7e39fe064.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7556dbeff1459777d38015b0c7d74635",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "e81b6b90de85d40f36a8",
+    "url": "/monster-rolodex/static/css/main.e7d8df16.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/monster-rolodex/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monster-rolodex/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e81b6b90de85d40f36a8",
+    "url": "/monster-rolodex/static/js/main.c93d28e2.chunk.js"
+  },
+  {
+    "revision": "8575afebb5261fef1571",
+    "url": "/monster-rolodex/static/js/runtime-main.502e2e68.js"
+  }
+]);
